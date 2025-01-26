678f61ee7c20847c61d080a512e2d98e39c1101e
Ryan <ryangabryel2005@gmail.com>
2025-01-26 11:56:38 -0300

    some basic arrays operations and trying more stuff

--- introduction/index.js
+++ introduction/index.js
@@ -6,4 +6,25 @@ var firstPerson = {
         country: "United States",
     }
 };
+/*
+You can add optional properties by just putting a ? sign after the name it
+let firstPerson : Person = {
+    name : "John Doe",
+    age : 24,
+    address? : {
+        state : "California",
+        country: "United States",
+    }
+}
+
+Now address is not needed necessarily anymore
+*/
 console.log(firstPerson);
+//Syntax for arrays
+var persons = new Array();
+persons.push(firstPerson);
+for (var i = 0; i < persons.length; i++) {
+    console.log(persons[i]);
+}
+var numbers = [1, 2, 3, 4];
+numbers.push(true);
--- introduction/index.ts
+++ introduction/index.ts
@@ -18,4 +18,40 @@ let firstPerson : Person = {
     }
 }
 
+
+/*
+You can add optional properties by just putting a ? sign after the name it
+let firstPerson : Person = {
+    name : "John Doe", 
+    age : 24,
+    address? : {
+        state : "California",
+        country: "United States",
+    }
+}
+
+Now address is not needed necessarily anymore
+*/
+
+
 console.log(firstPerson)
+
+//Syntax for arrays
+
+let persons : Person[] = new Array()
+
+persons.push(firstPerson)
+
+for(let i : number = 0; i < persons.length;i++) {
+    console.log(persons[i])
+}
+
+/*
+Introducting a basic error using arrays
+let numbers = [1, 2, 3, 4]
+
+numbers.push(true)
+
+This error happens basically because typescript automatically assigns the number type to the array of numbers
+so when we try to put a value that is not a number we will get an error
+*/
